198de8906976004fdf0ef06834c4b1249763b036
chenxiong <snjer@foxmail.com>
2017-09-27 23:32:19 +0800

    Add file save function(0)

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,9 +1,9 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent), channel_flag(0),
+    QMainWindow(parent), channel_flag(0), wave_file(0), wave_offset(0),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
@@ -11,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent) :
     connect(this, SIGNAL(paintwave()), this, SLOT(on_paintwave()));
 
     for(int i = 0; i < ADC_CHANNEL_NUM; i++) {
+        file_index[i] = 0;
         adc_channel[i].resize(WAVE_X_LENGTH);
         adc_channel[i].fill(i / 2.0, WAVE_X_LENGTH);
         //向绘图区域QCustomPlot(从widget提升来的)添加一条曲线
@@ -36,16 +37,34 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::wave_push_back(QVector<double> &channel, short val)
+void MainWindow::wave_push_back(QVector<double> &channel, short val[], int num)
 {
-    channel.push_back(val * 2.5 /32768);
-    channel.pop_front();
+    for (int i = 0; i < num; i++) {
+        channel.push_back(val[i] * 2.5 /32768);
+        channel.pop_front();
+    }
+    if (num +wave_offset > FILE_SIZE) {
+       wave_file += 1;
+       wave_offset =wave_offset + num - FILE_SIZE;
+    }else {
+       wave_offset += num;
+    }
+    paintwave();
 }
 
-void MainWindow::wave_push_front(QVector<double> &channel, short val)
+void MainWindow::wave_push_front(QVector<double> &channel, short val[], int num)
 {
-    channel.push_front(val * 2.5 /32768);
-    channel.pop_back();
+    for (int i = 0; i < num; i++) {
+        channel.push_front(val * 2.5 /32768);
+        channel.pop_back();
+    }
+    if (offset - num < 0) {
+       wave_file -= 1;
+       wave_offset = FILE_SIZE +wave_offset - num;
+    } else {
+       wave_offset -= num;
+    }
+    paintwave();
 }
 
 void MainWindow::paintEvent(QPaintEvent *event)
@@ -66,4 +85,65 @@ void MainWindow::on_paintwave(void)
     //设置坐标轴显示范围,否则我们只能看到默认的范围
     ui->wave_wdt->xAxis->setRange(0, x.size());
     ui->wave_wdt->replot();
+}
+
+void MainWindow::file_write(unsigned char channel, short val[], int num)
+{
+    if (channel >= ADC_CHANNEL_NUM && num <= 0)
+        return;
+    if (!file[channel].isOpen()) {
+        file[channel] = QFile(QString("channel_%1_%2").arg(channel).arg(file_index[channel]));
+        if (!file[channel].open(QIODevice::ReadOnly)) {
+            QMessageBox msg(QMessageBox::Warning, "Error", "Open file failed");
+            msg.exec();
+            return;
+        }
+    }
+
+    if (file[channel]->size() + (2 * num) >= FILE_SIZE) {
+        int count = FILE_SIZE - file[channel]->size();
+        file[channel]->write((const char*)val, count);
+        file[channel]->save();
+        file[channel]->close();
+        file_index[channel]++;
+        file[channel] = new QFile(QString("channel_%1_%2").arg(channel).arg(file_index[channel]));
+        if (!file[channel]) {
+            QMessageBox msg(QMessageBox::Warning, "Error", "Create file failed");
+            msg.exec();
+            return;
+        }
+        file[channel]->write((const char*)val, (2 * num) - count);
+    } else {
+        file[channel]->write((const char*)val, 2 * num);
+    }
+}
+
+void MainWindow::file_read(unsigned char channel, short val[], int num)
+{
+    if (channel >= ADC_CHANNEL_NUM && num <= 0)
+        return;
+
+    QFile temp = QFile(QString("channel_%1_%2").arg(channel).arg(wave_file));
+        if (!file[channel]) {
+            QMessageBox msg(QMessageBox::Warning, "Error", "Create file failed");
+            msg.exec();
+            return;
+        }
+    }
+
+    if (file[channel]->size() + (2 * num) >= FILE_SIZE) {
+        int count = FILE_SIZE - file[channel]->size();
+        file[channel]->write((const char*)val, count);
+        file[channel]->save();
+        file_index[channel]++;
+        file[channel] = new QFile(QString("channel_%1_%2").arg(channel).arg(file_index[channel]));
+        if (!file[channel]) {
+            QMessageBox msg(QMessageBox::Warning, "Error", "Create file failed");
+            msg.exec();
+            return;
+        }
+        file[channel]->write((const char*)val, (2 * num) - count);
+    } else {
+        file[channel]->write((const char*)val, 2 * num);
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -3,9 +3,12 @@
 
 #include <QMainWindow>
 #include <QVector>
+#include <QFile>
 
 #define ADC_CHANNEL_NUM 6
-#define WAVE_X_LENGTH 512
+#define ADC_CHANNEL(val)   ((0x01) << (val))
+#define WAVE_X_LENGTH 1000
+#define FILE_SIZE   (10 * 1024 * 1024)
 
 namespace Ui {
 class MainWindow;
@@ -20,19 +23,29 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_paintwave(void);
+    void on_paintwave(void);    //画波形函数
 
 signals:
-    void paintwave(void);
+    void paintwave(void);   //画波形图信号
 
 private:
     Ui::MainWindow *ui;
-    QVector<double> adc_channel[ADC_CHANNEL_NUM];
-    QVector<double> x;
-    unsigned char channel_flag;
+    QFile file[ADC_CHANNEL_NUM];
+    QVector<double> adc_channel[ADC_CHANNEL_NUM];   //显示Y轴坐标
+    QVector<double> x;          //显示X轴坐标
+    unsigned char channel_flag; //显示通道MASK
+    unsigned long wave_file;    //当前显示数据的文件索引
+    unsigned long wave_offset;  //当前显示数据的文件内偏移
+    unsigned long file_index[ADC_CHANNEL_NUM];         //当前存储文件编号
 
-    void wave_push_back(QVector<double> &channel, short val);
-    void wave_push_front(QVector<double> &channel, short val);
+    /* 向波形后面添加数据 */
+    void wave_push_back(QVector<double> &channel, short val[], int num);
+    /* 向波形前面添加数据 */
+    void wave_push_front(QVector<double> &channel, short val[], int num);
+    /* 存储数据到文件 */
+    void file_write(unsigned char channel, short val[], int num);
+    /* 从文件中读取数据 */
+    void file_read(unsigned char channel, short val[], int num);
     void paintEvent(QPaintEvent *event);
 };
 
